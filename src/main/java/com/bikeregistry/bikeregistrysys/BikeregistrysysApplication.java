f5214821638b846ffaa10c933ea18fc7eea3a83e
Shridatta <sgz2@njit.edu>
2019-02-10 17:15:44 -0500

    added bike endpoint

---
+++ src/main/java/com/bikeregistry/bikeregistrysys/BikeregistrysysApplication.java
@@ -0,0 +1,14 @@
+package com.bikeregistry.bikeregistrysys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeregistrysysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeregistrysysApplication.class, args);
+	}
+
+}
+
