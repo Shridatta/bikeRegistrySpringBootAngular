bd8435dcde0e59a96e7f75996ed0cafd8ad96682
Shridatta <sgz2@njit.edu>
2019-02-10 20:08:35 -0500

    creating JPA repository

--- src/main/java/com/bikeregistry/bikeregistrysys/controllers/BikeController.java
+++ src/main/java/com/bikeregistry/bikeregistrysys/controllers/BikeController.java
@@ -1,7 +1,9 @@
 package com.bikeregistry.bikeregistrysys.controllers;
 
 
+import com.bikeregistry.bikeregistrysys.Repositories.BikeRepository;
 import com.bikeregistry.bikeregistrysys.models.Bike;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,21 +13,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/bikes")
 public class BikeController {
+
+    @Autowired
+    private BikeRepository bikeRepository;
+
     @GetMapping
     public List<Bike> list(){
-        List<Bike> bikes = new ArrayList<>();
-            return bikes;
+           return bikeRepository.findAll();
         }
 
         @PostMapping
         @ResponseStatus(HttpStatus.OK)
 
         public void create(@RequestBody Bike bike) {
+            bikeRepository.save(bike);
         }
 
         @GetMapping("/{id}")
         public Bike get(@PathVariable("id") long id) {
-         return new Bike();
+         return bikeRepository.getOne(id);
         }
 
 }
