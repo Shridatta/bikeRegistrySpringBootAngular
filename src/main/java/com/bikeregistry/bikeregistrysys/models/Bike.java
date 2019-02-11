02d743e24f67abb1c15a0fd4b1e17c849e186108
Shridatta <sgz2@njit.edu>
2019-02-10 20:00:13 -0500

    creating JPA entity

--- src/main/java/com/bikeregistry/bikeregistrysys/models/Bike.java
+++ src/main/java/com/bikeregistry/bikeregistrysys/models/Bike.java
@@ -1,9 +1,29 @@
 package com.bikeregistry.bikeregistrysys.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Entity
 public class Bike {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
     private String name;
     private String email;
     private String phone;
